464c187a3f4ac9861df24d4b74e417c3856a84cc
vasantivmahajan <mahajan.v@husky.neu.edu>
2017-02-27 19:37:50 -0500

    modularized the code, added the merging code for the entire batch of dataframe

--- Part2_EDGAR_LogDataset.py
+++ Part2_EDGAR_LogDataset.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[7]:
+# In[2]:
 
 ###from bs4 import BeautifulSoup
 import requests
@@ -9,15 +9,16 @@ import pandas as pd
 import numpy as np
 #import urllib3 as ur
 import urllib as ur
-
+import configparser
 import os.path
 import zipfile
 import tinys3
+import sys
 
 
-# In[8]:
+# In[ ]:
 
-import configparser
+
 Config = configparser.ConfigParser()
 Config.read('config.ini')
 Config.sections()
@@ -55,55 +56,16 @@ def fetch_company_name_cik_table():
     ###return df
 
 
-# In[10]:
+# In[16]:
 
 #!/usr/bin/env python       
-
+merged_dataframe=pd.DataFrame()
 class GetData:
-    
+   
     def __init__(self):
         """
         Retrieves and stores the urllib.urlopen object for a given url
         """
-        
-    def generate_url(self,year):
-        
-        #generate the url for fetching the log files for every month's first day
-        number_of_months=1
-        while number_of_months < 13:
-            if(number_of_months <10):
-                url="http://www.sec.gov/dera/data/PublicEDGAR-log-file-data/"+year+"/Qtr1/log"+year+'%02d' % number_of_months+"01.zip"
-            else:
-                url="http://www.sec.gov/dera/data/PublicEDGAR-log-file-data/"+year+"/Qtr1/log"+year+str(number_of_months)+"01.zip"
-            number_of_months=number_of_months+1
-        #temp_url=download_data("http://www.sec.gov/dera/data/Public-EDGAR-log-file-data/2016/Qtr1/log20160101.zip")
-        return self.download_data("http://www.sec.gov/dera/data/Public-EDGAR-log-file-data/2003/Qtr1/log20030301.zip")
-        
-    def download_data(self,url):
-
-        #fetching the zip file name from the URL
-        file_name=url.split("/")
-        
-        self.create_directory("Part_2_log_datasets")
-
-        #Downloading data if not already present in the cache
-        if(os.path.exists("Part_2_log_datasets/"+file_name[8])):
-            print("Already present")
-
-        else:
-            ur.request.urlretrieve(url, "Part_2_log_datasets/"+file_name[8])
-            print("Download complete")
-
-        #unzip the file and fetch the csv file
-        zf = zipfile.ZipFile("Part_2_log_datasets/"+file_name[8]) 
-        csv_file_name=file_name[8].replace("zip", "csv")
-        zf_file=zf.open(csv_file_name)
-
-        #create a dataframe from the csv
-        df = pd.read_csv(zf_file)
-        
-        return df
-    
     def create_directory(self,path):
         try:
             if not os.path.exists(path):
@@ -111,6 +73,88 @@ class GetData:
         except OSError as exception:
             if exception.errno != errno.EEXIST:
                 raise
+    
+    def setDataFrame(self, df):
+        merged_dataframe = df
+        
+    def getDataFrame(self):
+        return merged_dataframe
+    
+    def maybe_download(self, url_list, year):
+   
+        list=['df1','df2','df3','df4','df5','df6','df7','df8','df9','df10','df11','df12']
+        year=str(year)
+        count=0
+        for i in url_list:
+
+            #fetching the zip file name from the URL
+            file_name=i.split("/")
+            self.create_directory("Part_2_log_datasets_trial/"+year+"/")
+
+            #Downloading data if not already present in the cache
+            if(os.path.exists("Part_2_log_datasets_trial/"+year+"/"+file_name[8])):
+                print("Data for ",file_name[8]," is already present, pulling it from cache")
+
+            else:
+                #pbar = ProgressBar(widgets=[Percentage(), Bar()])
+
+
+                urllib.request.urlretrieve(i, "Part_2_log_datasets_trial/"+year+"/"+file_name[8], reporthook)
+                print("Data for ",file_name[8],"not present in cache. Downloading data")
+
+            #unzip the file and fetch the csv file
+            zf = zipfile.ZipFile("Part_2_log_datasets_trial/"+year+"/"+file_name[8]) 
+            csv_file_name=file_name[8].replace("zip", "csv")
+            zf_file=zf.open(csv_file_name)
+
+            #create a dataframe from the csv and append it to the list of dataframe
+            list[count]=pd.read_csv(zf_file)
+            count=count+1  
+        #merging the data into one dataframe
+        merged_dataframe=pd.concat([list[0],list[1],list[2],list[3],list[4],list[5],list[6],list[7],list[8],list[9],list[10],list[11]], ignore_index=True)
+        self.setDataFrame(merged_dataframe)
+        return merged_dataframe
+    
+
+    def generate_url(self, year):
+        url_list=list()
+        #generate the url for fetching the log files for every month's first day
+        number_of_months=1
+
+        while number_of_months < 13:
+            #find the quarter for the month
+            if number_of_months >= 1 and number_of_months < 4:
+                quarter="Qtr1"
+            elif(number_of_months >= 4 and number_of_months < 7):
+                quarter="Qtr2"
+            elif(number_of_months >= 7 and number_of_months < 10):
+                quarter="Qtr3"
+            elif(number_of_months >= 10 and number_of_months < 13):
+                quarter="Qtr4"
+
+            if(number_of_months <10):
+                url="http://www.sec.gov/dera/data/Public-EDGAR-log-file-data/"+str(year)+"/"+quarter+"/log"+str(year)+'%02d' % number_of_months+"01.zip"
+
+            else:
+                url="http://www.sec.gov/dera/data/Public-EDGAR-log-file-data/"+str(year)+"/"+quarter+"/log"+str(year)+str(number_of_months)+"01.zip"
+
+            url_list.append(url)
+            number_of_months=number_of_months+1
+
+        return self.maybe_download(url_list,year)
+        
+    def fetch_year(self):
+    
+         #fetch the year for which the user wants logs
+        year = input('Enter the year for which you need to fetch the log files: ')
+        year=int(year)
+        if(year >= 2003 and year < 2016):
+            #calling the function to generate dynamic URL
+            return self.generate_url(year)
+        else:
+            print("EDGAR log files are available for years 2003-2016. Kindly enter a year within this range")
+            fetch_year()
+    
                 
     def create_zip_folder(self,path):
         zipfolder_name=path+'.zip'
@@ -132,104 +176,156 @@ class GetData:
 
         # Uploading a single file
         f = open("Part_2_log_datasets.zip",'rb')
-        conn.upload("Part_2_log_datasets.zip",f,BUCKET_NAME)             
+        conn.upload("Part_2_log_datasets.zip",f,BUCKET_NAME)  
         
-#fetch the year for which the user wants logs
-year = input('Enter the year for which you need to fetch the log files: ')
-#calling the function to generate dynamic URL
 get_data_obj=GetData()
-df=get_data_obj.generate_url(year)
+merged_dataframe=get_data_obj.fetch_year()
+#fetch the year for which the user wants logs
+#year = input('Enter the year for which you need to fetch the log files: ')
+#calling the function to generate dynamic URL
+
+#df=get_data_obj.generate_url(year)
         
-get_data_obj.create_zip_folder("Part_2_log_datasets")
-get_data_obj.upload_zip_to_s3("Part_2_log_datasets.zip")
+#get_data_obj.create_zip_folder("Part_2_log_datasets")
+#get_data_obj.upload_zip_to_s3("Part_2_log_datasets.zip")
 
-
-# In[ ]:
-
-#convert all the integer column in int format
-
-df['zone'] = df['zone'].astype('int')
-df['cik'] = df['cik'].astype('int')
-df['code'] = df['code'].astype('int')
-df['idx']=df['idx'].astype('int')
-df['norefer']=df['norefer'].astype('int')
-df['noagent']=df['noagent'].astype('int')
-df['find']=df['find'].astype('int')
-df['crawler']=df['crawler'].astype('int')
-#print(df.head(25))
-
-
-# In[ ]:
-
-
-#replacing empty strings with NaN 
-df.replace(r'\s+', np.nan, regex=True)
-
-
-# In[ ]:
-
-
-#replace all ip column NaN value by a default ip address 
-df["ip"].fillna("255.255.255.255", inplace=True)
-
-#perform forward fill to replace NaN values by fetching the next valid value
-df["date"].fillna(method='ffill')
-
-#perform backward fill to replace NaN values by backpropagating and fetching the previous valid value
-df["time"].fillna(method='bfill')
-
-#replace all zone column NaN values by 'Not Available' extension
-df["zone"].fillna("Not Available", inplace=True)
-
-#replace all extension column NaN values by default extension
-df["extention"].fillna("-index.htm", inplace=True)
-
-#replace all size column NaN values by 0 and convert the column into integer 
-df["size"].fillna(0, inplace=True)
-df['size'] = df['size'].astype('int')
-
-#replace all user agent column NaN values by the default value 1 (no user agent)
-df["noagent"].fillna("Not Applicable", inplace=True)
-
-#replace all find column NaN values by the default value 0 (no character strings found)
-df["find"].fillna(0, inplace=True)
-
-#replace all broser column NaN values by a string
-df["browser"].fillna("Not Available", inplace=True)
-#df
-
-
-# In[ ]:
-
-# if the value in idx column is missing, check the value of the extension column, if its "-index.html" set the column's value 1 else 0
-count=0
-for i in df['idx']:
-    if(np.isnan(i)):
-        if(df['extension'][count]=="-index.htm"):
-            i=1
-        else:
-            i=0
-    count=count+1
-
-# if the value of norefer column is missing, check the value of the find column, if it is 0, set the value 1, else it set it 0
-counter=0
-for i in df['norefer']:
-    if(np.isnan(i)):
-        if(df["find"][counter]==0):
-            i=1
-        else:
-            i=0
-    counter=counter+1
+class Process_and_analyse_data():
     
-# if the value of crawler is missing, check the value of the code, if it is 404 set it as 1 else 0
-count_position=0
-for i in df['crawler']:
-    if(np.isnan(i)):
-        if(df["code"][count_position]==404):
-            i=1
-        else:
-            i=0
-    count_position=count_position+1
+    def __init__(self):
+        """
+        Retrieves and stores the urllib.urlopen object for a given url
+        """
+    
+    def format_dataframe_columns(self):
+        #convert all the integer column in int format
+
+        df['zone'] = df['zone'].astype('int')
+        df['cik'] = df['cik'].astype('int')
+        df['code'] = df['code'].astype('int')
+        df['idx']=df['idx'].astype('int')
+        df['norefer']=df['norefer'].astype('int')
+        df['noagent']=df['noagent'].astype('int')
+        df['find']=df['find'].astype('int')
+        df['crawler']=df['crawler'].astype('int')
+        
+        #replacing empty strings with NaN 
+        df.replace(r'\s+', np.nan, regex=True)
+        self.handle_nan_values()
+        
+    def handle_nan_values(self):
+        
+        #replace all ip column NaN value by a default ip address 
+        df["ip"].fillna("255.255.255.255", inplace=True)
+
+        #perform forward fill to replace NaN values by fetching the next valid value
+        df["date"].fillna(method='ffill')
+
+        #perform backward fill to replace NaN values by backpropagating and fetching the previous valid value
+        df["time"].fillna(method='bfill')
+
+        #replace all zone column NaN values by 'Not Available' extension
+        df["zone"].fillna("Not Available", inplace=True)
+
+        #replace all extension column NaN values by default extension
+        df["extention"].fillna("-index.htm", inplace=True)
+
+        #replace all size column NaN values by 0 and convert the column into integer 
+        df["size"].fillna(0, inplace=True)
+        df['size'] = df['size'].astype('int')
+
+        #replace all user agent column NaN values by the default value 1 (no user agent)
+        df["noagent"].fillna("Not Applicable", inplace=True)
+
+        #replace all find column NaN values by the default value 0 (no character strings found)
+        df["find"].fillna(0, inplace=True)
+
+        #replace all broser column NaN values by a string
+        df["browser"].fillna("Not Available", inplace=True)
+        
+        # if the value in idx column is missing, check the value of the extension column, if its "-index.html" set the column's value 1 else 0
+        count=0
+        for i in df['idx']:
+            if(np.isnan(i)):
+                if(df['extension'][count]=="-index.htm"):
+                    i=1
+                else:
+                    i=0
+            count=count+1
+
+        # if the value of norefer column is missing, check the value of the find column, if it is 0, set the value 1, else it set it 0
+        counter=0
+        for i in df['norefer']:
+            if(np.isnan(i)):
+                if(df["find"][counter]==0):
+                    i=1
+                else:
+                    i=0
+            counter=counter+1
+
+        # if the value of crawler is missing, check the value of the code, if it is 404 set it as 1 else 0
+        count_position=0
+        for i in df['crawler']:
+            if(np.isnan(i)):
+                if(df["code"][count_position]==404):
+                    i=1
+                else:
+                    i=0
+            count_position=count_position+1
+        self.fetch_company_name_from_cik()
+    
+    def fetch_company_name_from_cik(self):
+        #we found a list of CIK their company names from a EDGAR's github repository. We are fetching this information to gain the information about company name
+        company_df = pd.read_csv('CIK-mapping.csv') 
+        #renaming column, so that both the dataframes can be merged on the common column
+        company_df = company_df.rename(columns={'CIK': 'cik'})
+        company_df['cik'] = company_df['cik'].astype('int')
+        #merging both the dataframes
+        merged_df_cik_company_name= df.join(company_df, on='cik', how='left', rsuffix="_review")
+        #merged_df_cik_company_name=pd.merge(df, company_df, on='cik', how='left')
+        #merged_df_cik_company_name=pd.merge(df,company_df, left_on='cik',right_on='CIK' )
+        print(merged_df_cik_company_name.head(10))
+        
+    def identify_cik_accession_number_anomaly(self):
+        #insert a column to check CIK, Accession number discripancy
+        df.insert(6, "CIK_Accession_Anamoly_Flag", "N")
+                
+        #check if CIK and Accession number match. The Accession number is divided into three parts, CIK-Year-Number_of_filings_listed.
+        #the first part i.e the CIK must match with the CIK column. If not, there exists an anomaly
+
+        count=0;
+        print("Creating CIK_Accession_Anomaly_Flag column")
+        for i in df['accession']:
+            #fetch the CIK number from the accession number and convert it into integer
+            list_of_fetched_cik_from_accession=[(int(i.split("-")[0]))]
+
+            #check if the CIK number from the column and CIK number fetched from the accession number are equal
+            if(df['cik'][count]!=list_of_fetched_cik_from_accession):
+                df['CIK_Accession_Anamoly_Flag'][count]="Y"
+
+            count=count+1
+        print("Done")
+        print(df.head(10))
+        
+    def get_file_name_from_extension():
+        df.insert(7, "filename", "")
+        #Extension rule: if the file name is missing and only the file extension is present, then the file name is document accession number
+        count=0
+        for i in df["extention"]:
+            if(i==".txt"):
+                # if the value in extension is only .txt, fetch the accession number and append accession number to .txt
+                #list_of_fetched_cik_from_accession=int(((df2["accession"].str.split("-")[count])[0]))
+                #print((df["accession"]).astype(str))
+                #list_of_fetched_cik_from_accession=int(df["accession"])
+                df["filename"][count]=(df["accession"][count])+".txt" 
+            else:
+                df["filename"][count]=i
+            count=count+1
+        
+        
+get_data_obj=GetData()
+df=get_data_obj.getDataFrame()
+process_data_obj=Process_and_analyse_data()
+process_data_obj.format_dataframe_columns()
 
 
 # In[ ]:
@@ -298,7 +394,7 @@ for i in df["extention"]:
 #print(df.head(10))
 
 
-# In[6]:
+# In[ ]:
 
 ConfigSectionMap("Part_1")
 
